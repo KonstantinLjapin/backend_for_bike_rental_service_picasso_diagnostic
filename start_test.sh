ea7f83ab246f2cadc337d1e9818a798771bcb5c9
lyapin <lajpinkonstantin@gmail.com>
2024-07-30 12:13:32 +0300

    start proj

---
+++ start_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# need chmod +
+sudo docker compose up;
+sudo docker stop $(sudo docker ps -a -q);
+sudo docker rm $(sudo docker ps -a -q);
+sudo docker rmi $(sudo docker images --format="{{.Repository}} {{.ID}}" |
+                  grep "^backend_for_bike_rental_service_picasso_diagnostic-django_api" | cut -d' ' -f2);
+sudo rm -r dump/;
