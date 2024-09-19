0514e7fd01d872f326d44604c72f8af294303349
lyapin <lajpinkonstantin@gmail.com>
2024-09-19 15:49:27 +0300

    add nginx config

--- start_test.sh
+++ start_test.sh
@@ -9,4 +9,7 @@ sudo docker rmi $(sudo docker images --format="{{.Repository}} {{.ID}}" |
                   grep "^backend_for_bike_rental_service_picasso_diagnostic-celery_worker" | cut -d' ' -f2);
 sudo docker rmi $(sudo docker images --format="{{.Repository}} {{.ID}}" |
                   grep "^backend_for_bike_rental_service_picasso_diagnostic-django_test" | cut -d' ' -f2);
+sudo docker rmi $(sudo docker images --format="{{.Repository}} {{.ID}}" |
+                  grep "^backend_for_bike_rental_service_picasso_diagnostic-nginx" | cut -d' ' -f2);
+sudo docker network rm backend_for_bike_rental_service_picasso_diagnostic_backend;
 sudo rm -r dump/;
